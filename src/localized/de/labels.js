878993aa6c09217490df3a2cd78410ee4ba25694
Jan Amann <jan@amann.me>
2019-12-13 16:07:50 +0100

    Improve site description.

--- src/localized/de/labels.js
+++ src/localized/de/labels.js
@@ -2,5 +2,5 @@ export default {
   title: 'Jan Amann',
   tagline: 'Partner für User Interface Entwicklung und Design',
   description:
-    'Meine Leidenschaft ist die Entwicklung von webbasierten Produkten mit besonderem Anspruch an Design, Usability und Performance. Dazu setze ich auf Technologien wie React, node.js und GraphQL.'
+    'Meine Leidenschaft ist die Entwicklung von webbasierten Produkten mit besonderem Anspruch an Design, Usability und Performance. Ich bin technologisch spezialisiert auf React, node.js und GraphQL.'
 };
