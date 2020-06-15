0c214e4d561903a2479e863de32cb0d8e809f3e1
Jan Amann <jan@amann.me>
2020-06-15 14:55:36 +0200

    Pre-load project images.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,12 @@
 import React, {Children, useState, cloneElement} from 'react';
 import {motion} from 'framer-motion';
 import Wrapper from 'components/Wrapper';
+import styles from './Projects.module.scss';
 
 export default function Projects({children, pageSize = 3, showMoreButton}) {
   const [limit, setLimit] = useState(pageSize);
   const projects = Children.toArray(children);
-  const paginatedNodes = projects.slice(0, limit);
-  const hasMore = paginatedNodes.length < projects.length;
+  const hasMore = limit < projects.length;
 
   function onShowMore() {
     setLimit(limit + pageSize);
@@ -14,22 +14,22 @@ export default function Projects({children, pageSize = 3, showMoreButton}) {
 
   return (
     <>
-      {paginatedNodes.map((node, index) => (
-        <motion.div
-          key={index}
-          animate={{
-            opacity: 1,
-            y: 0
-          }}
-          initial={{
-            opacity: 0,
-            y: 100
-          }}
-          transition={{duration: 0.5}}
-        >
-          {node}
-        </motion.div>
-      ))}
+      {projects.map((node, index) => {
+        // Invisible projects are pre-rendered so the images are already loaded.
+        const isVisible = index < limit;
+
+        return (
+          <motion.div
+            key={index}
+            animate={isVisible ? {opacity: 1, y: 0} : undefined}
+            className={isVisible ? undefined : styles.hidden}
+            initial={{opacity: 0, y: 100}}
+            transition={{duration: 0.5}}
+          >
+            {node}
+          </motion.div>
+        );
+      })}
       {hasMore && (
         <Wrapper>{cloneElement(showMoreButton, {onClick: onShowMore})}</Wrapper>
       )}
