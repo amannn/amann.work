9000ec595405a936e33ad3062d7d8877db5cb432
Jan Amann <jan@amann.me>
2020-09-21 09:42:15 +0200

    Minor wording update.

--- graphql/getOpenSourceContributions.js
+++ graphql/getOpenSourceContributions.js
@@ -9,7 +9,8 @@ const PR_REPO_OWNER_BLACKLIST = ['amannn', 'molindo', 'tools-aoeur'];
 // Not particularly interesting PRs.
 const PR_ID_BLACKLIST = [
   'MDExOlB1bGxSZXF1ZXN0MTkyNTAwNjUz',
-  'MDExOlB1bGxSZXF1ZXN0MzYwOTI4MzYw'
+  'MDExOlB1bGxSZXF1ZXN0MzYwOTI4MzYw',
+  'MDExOlB1bGxSZXF1ZXN0NDc0Nzk1MDc3'
 ];
 
 const query = /* GraphQL */ `
