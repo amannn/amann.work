0f3dcdb764250ce2afc2e1e30276e6e073756662
Jan Amann <jan@amann.me>
2020-11-06 19:15:58 +0100

    Cache fonts.

--- next.config.js
+++ next.config.js
@@ -13,7 +13,18 @@ module.exports = withMdxEnhanced(
       i18n: {
         locales: ['en', 'de'],
         defaultLocale: 'en'
-      }
+      },
+      headers: () => [
+        {
+          source: '/fonts/(.*)',
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'public,max-age=31536000,immutable'
+            }
+          ]
+        }
+      ]
     })
   )
 );
