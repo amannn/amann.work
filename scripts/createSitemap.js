46cc6b08306632e922971fedb3254e2a41d639c8
Jan Amann <jan@amann.me>
2020-11-07 07:58:08 +0100

    Create sitemap.

---
+++ scripts/createSitemap.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const {configureSitemap} = require('@sergeymyssak/nextjs-sitemap');
+
+configureSitemap({
+  baseUrl: 'https://amann.me',
+  langs: ['en', 'de'],
+  excludeExtensions: ['scss', 'mp4', 'png'],
+  targetDirectory: path.join(process.cwd(), 'public'),
+  pagesDirectory: path.join(process.cwd(), 'src/pages')
+})
+  .generateSitemap()
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('Sitemap created\n');
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
