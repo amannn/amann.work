fc707eccf1050a812c51a763ddbcb5cec8f88b59
Jan Amann <jan@amann.me>
2020-11-20 09:48:39 +0100

    Fix `useSsr`

--- src/hooks/useSsr.js
+++ src/hooks/useSsr.js
@@ -2,6 +2,14 @@ import {createContext, useContext} from 'react';
 
 export const IsSsrContext = createContext();
 
+let hasMounted = false;
+if (typeof window !== 'undefined') {
+  setTimeout(() => {
+    hasMounted = true;
+  }, 2000);
+}
+
 export default function useSsr() {
-  return useContext(IsSsrContext);
+  const isSsrContext = useContext(IsSsrContext);
+  return hasMounted ? false : isSsrContext;
 }
