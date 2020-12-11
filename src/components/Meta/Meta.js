760245d8158dcf3c138d6f0eb4cb1d2ca79e748b
Jan Amann <jan@amann.me>
2020-12-11 15:26:29 +0100

    Add meta tags

--- src/components/Meta/Meta.js
+++ src/components/Meta/Meta.js
@@ -4,12 +4,17 @@ import Head from 'next/head';
 export default function Meta({description, title}) {
   const t = useTranslations('Meta');
 
+  const finalTitle = [title, t('title')]
+    .filter((part) => part != null)
+    .join(' - ');
+  const finalDescription = description || t('description');
+
   return (
     <Head>
-      <title>
-        {[title, t('title')].filter((part) => part != null).join(' - ')}
-      </title>
-      <meta content={description || t('description')} name="description" />
+      <title>{finalTitle}</title>
+      <meta content={finalDescription} name="description" />
+      <meta content={finalTitle} property="og:title" />
+      <meta content={finalDescription} property="og:description" />
     </Head>
   );
 }
